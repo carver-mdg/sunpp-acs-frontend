b055d53c82d4cf6198a17b4867e7bd8f18ac3a98
carver.mdg <carver.mdg@gmail.com>
2022-08-15 14:04:30 +0300

    @fix ppr protocol file extension

--- src/stores/ppr.js
+++ src/stores/ppr.js
@@ -124,7 +124,7 @@ export const usePprsStore = defineStore('pprs', {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement("a");
           link.href = url;
-          link.setAttribute("download", `${ppr.name}-${utils.generateUniqueID()}.mp3`);
+          link.setAttribute("download", `${ppr.name}-${utils.generateUniqueID()}.zip`);
           document.body.appendChild(link);
           link.click();
         })
